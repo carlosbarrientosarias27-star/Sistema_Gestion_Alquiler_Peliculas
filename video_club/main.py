3eee71a2114ca207e95a3d279ec76b42b7f030b8
carlosbarrientosarias27-star <carlos.barrientosarias27@gmail.com>
2026-04-16 11:32:17 +0200

    añadir en el main.py en linea 2 (sys.path)

--- video_club/main.py
+++ video_club/main.py
@@ -2,9 +2,7 @@ import sys
 import os
 
 # Añade la carpeta actual al path de búsqueda de Python
-directorio_actual = os.path.dirname(os.path.abspath(__file__))
-if directorio_actual not in sys.path:
-    sys.path.append(directorio_actual)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from database.init_db import init_db
 from ui.menu import Menu
